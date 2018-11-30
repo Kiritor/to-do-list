86220f61f780614fc33b90de7f812ae0484bf4d8
LCore <1406754158@qq.com>
2018-11-30 16:48:48 +0800

    添加readme

--- js/to-do.js
+++ js/to-do.js
@@ -4,22 +4,26 @@
         el: "#todo-app",
         data: {
             newTodoItem: '',
-            todoItems: [{
-                title: '写博客',
-                completed: true
-            }]
+            todoItems: []
         },
         computed: {
             //未完成的数量
             activeCount() {
                 return this.todoItems.filter(item => !item.completed).length;
             },
+            //已完成数量
+            completedCount(){
+                return this.todoItems.filter(item => item.completed).length;
+            },
+            showTodo() {
+                return this.todoItems.length > 0;
+            },
             allDone: {
                 get() {
                     return this.activeCount === 0;
                 },
                 set(value) {
-                    console.info(this.todoItems)
+                    console.info(value)
                     this.todoItems.map(item => {
                         item.completed = value
                     })
@@ -35,13 +39,27 @@
 
                 this.todoItems.unshift({
                     title: this.newTodoItem,
-                    completed: false
+                    completed: false,
+                    edited: false
                 })
                 this.newTodoItem = '';
             },
             removeTodoItem(item) {
                 var index = this.todoItems.indexOf(item);
                 this.todoItems.splice(index, 1);
+            },
+            removeCompletedItems() {
+                this.todoItems = this.todoItems.filter(item => !item.completed);
+            },
+            cancelEditItem(item) {
+                item.edited = false;
+            },
+            doneEditItem(item,index) {
+                item.edited = false;
+
+            },
+            editTodoItem(item) {
+                item.edited = true;
             }
 
         }
