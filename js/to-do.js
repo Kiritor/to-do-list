ac4d0eb9bb1d669e51eca48dc9c8f2982576f03f
LCore <1406754158@qq.com>
2018-12-06 15:18:35 +0800

    完善功能,引入axios添加后端持久化

--- js/to-do.js
+++ js/to-do.js
@@ -12,7 +12,7 @@
                 return this.todoItems.filter(item => !item.completed).length;
             },
             //已完成数量
-            completedCount(){
+            completedCount() {
                 return this.todoItems.filter(item => item.completed).length;
             },
             showTodo() {
@@ -30,23 +30,40 @@
                 }
             }
         },
+        created: function () {
+            this.initData();
+        },
         methods: {
+            initData() {
+                var that = this;
+                axios.get('http://localhost:8080/springboot/items/')
+                    .then(response => {this.todoItems = response.data});
+            },
+            updateData() {
+                axios.post('http://localhost:8080/springboot/items/update',this.todoItems)
+                .then();
+            },
+            deleteData(item) {
+                axios.post('http://localhost:8080/springboot/items/delete',item)
+                .then();
+            },
             addTodoItem(event) {
-
                 if (!this.newTodoItem) {
                     return;
                 }
-
                 this.todoItems.unshift({
                     title: this.newTodoItem,
                     completed: false,
                     edited: false
                 })
+                // this.updateData();
                 this.newTodoItem = '';
             },
             removeTodoItem(item) {
                 var index = this.todoItems.indexOf(item);
                 this.todoItems.splice(index, 1);
+                axios.post('http://localhost:8080/springboot/items/delete',item)
+                .then();
             },
             removeCompletedItems() {
                 this.todoItems = this.todoItems.filter(item => !item.completed);
@@ -54,15 +71,44 @@
             cancelEditItem(item) {
                 item.edited = false;
             },
-            doneEditItem(item,index) {
+            doneEditItem(item, index) {
                 item.edited = false;
+                this.updateData();
 
             },
             editTodoItem(item) {
                 item.edited = true;
             }
 
+        },
+        // 指令集合
+        directives: {
+            focus: {
+                update(el) {
+                    el.focus()
+                }
+            }
+        },
+        // 属性观察
+        
+        watch: {
+            /*
+            todoItems: {
+                deep: true,
+                handler(newValue, oldValue) {
+                axios.post('http://localhost:8080/springboot/items/update',newValue)
+                .then();
+            　}
+            }
+            */
+            completedCount: {
+            handler(newValue, oldValue) {
+                axios.post('http://localhost:8080/springboot/items/update',this.todoItems)
+                .then();
+            　}
+            }
         }
-    });
+    
+        });
 
 })();
